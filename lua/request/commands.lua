f31f00d754390b45d2f2d5ce613a9b9d69891847
Konstantin Greif <konstantin.greif@mailfence.com>
2024-07-23 20:22:39 +0200

    Modularize commands

---
+++ lua/request/commands.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.get = function(url)
+  local command = "curl " .. url
+  local handle = io.popen(command)
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+M.post = function(url, params)
+  local curl_params = ''
+  if params then
+    for key, value in pairs(params) do
+      curl_params = curl_params .. key .. '=' .. value .. '&'
+    end
+  end
+  local command = "curl -X POST --data \"" .. curl_params .. "\" " .. url
+  local handle = io.popen(command)
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+return M
--- lua/request/init.lua
+++ lua/request/init.lua
@@ -1,29 +1,12 @@
 local M = {}
 
+local commands = require('request.commands')
+
 -- M.setup = function(opts)
 --   print("Options:", opts)
 -- end
 
-M.get = function(url)
-  local command = "curl " .. url
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  handle:close()
-  return result
-end
-
-M.post = function(url, params)
-  local curl_params = ''
-  if params then
-    for key, value in pairs(params) do
-      curl_params = curl_params .. key .. '=' .. value .. '&'
-    end
-  end
-  local command = "curl -X POST --data \"" .. curl_params .. "\" " .. url
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  handle:close()
-  return result
-end
+M.get = commands.get
+M.post = commands.post
 
 return M
