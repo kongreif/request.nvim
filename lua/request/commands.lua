a53b142a7a49a8f321b25b6795f9b0ace1c66340
Konstantin Greif <konstantin.greif@mailfence.com>
2024-09-08 15:28:11 +0200

    Refactor code with lua_ls

--- lua/request/commands.lua
+++ lua/request/commands.lua
@@ -1,35 +1,41 @@
 local M = {}
 
 M.get = function(url)
-  local command = "curl -s " .. url
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  handle:close()
-  return result
+	local command = "curl -s " .. url
+	local handle = io.popen(command)
+	if handle == nil then
+		error("Nil response for command: " .. command)
+	end
+	local result = handle:read("*a")
+	handle:close()
+	return result
 end
 
 M.post = function(url, params)
-  local curl_params = ''
+	local curl_params = ""
 
-  if type(params) == "string" then
-    curl_params = params
-  elseif type(params) == "table" then
-    local param_pairs = {}
-    for key, value in pairs(params) do
-      table.insert(param_pairs, string.format('"%s": "%s"', key, tostring(value)))
-    end
-    curl_params = "{" .. table.concat(param_pairs, ", ") .. "}"
-  else
-    error("Invalid params: expected a table or a JSON string")
-  end
+	if type(params) == "string" then
+		curl_params = params
+	elseif type(params) == "table" then
+		local param_pairs = {}
+		for key, value in pairs(params) do
+			table.insert(param_pairs, string.format('"%s": "%s"', key, tostring(value)))
+		end
+		curl_params = "{" .. table.concat(param_pairs, ", ") .. "}"
+	else
+		error("Invalid params: expected a table or a JSON string")
+	end
 
-  local command = "curl -s -X POST -H 'Content-Type: application/json' --data \'" .. curl_params .. "\' " .. url
+	local command = "curl -s -X POST -H 'Content-Type: application/json' --data '" .. curl_params .. "' " .. url
 
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  handle:close()
+	local handle = io.popen(command)
+	if handle == nil then
+		error("Nil response for command: " .. command)
+	end
+	local result = handle:read("*a")
+	handle:close()
 
-  return result
+	return result
 end
 
 return M
